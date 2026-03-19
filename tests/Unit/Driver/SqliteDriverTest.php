de1c5a695f81d36eb43180a22b3a18eb1b3f62cf
sodaho <sodaho@mac.com>
2026-03-19 02:59:05 +0100

    test: remove redundant SQLite driver environment path tests

--- tests/Unit/Driver/SqliteDriverTest.php
+++ tests/Unit/Driver/SqliteDriverTest.php
@@ -10,11 +10,6 @@ use Sodaho\PdoWrapper\Exception\ConnectionException;
 
 class SqliteDriverTest extends TestCase
 {
-    protected function tearDown(): void
-    {
-        unset($_ENV['DB_SQLITE_PATH']);
-    }
-
     public function testDefaultsToMemory(): void
     {
         $driver = new SqliteDriver();
@@ -24,25 +19,6 @@ class SqliteDriverTest extends TestCase
 
     public function testExplicitMemoryPath(): void
     {
-        $driver = new SqliteDriver(':memory:');
-
-        $this->assertNotNull($driver->getPdo());
-    }
-
-    public function testReadsPathFromEnv(): void
-    {
-        $_ENV['DB_SQLITE_PATH'] = ':memory:';
-
-        $driver = new SqliteDriver();
-
-        $this->assertNotNull($driver->getPdo());
-    }
-
-    public function testExplicitPathOverridesEnv(): void
-    {
-        $_ENV['DB_SQLITE_PATH'] = '/some/invalid/path.db';
-
-        // Explicit :memory: should override ENV
         $driver = new SqliteDriver(':memory:');
 
         $this->assertNotNull($driver->getPdo());
